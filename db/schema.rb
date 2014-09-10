dc919266c202aa9d4fd78520eac3b5fe787ffaba
edsant1 <edwins10329@gmail.com>
2014-09-10 15:58:26 -0600

    latest update to iteration  -changed db from sql to postgres  -created nav bar  -added about link to nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140902231015) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: true do |t|
     t.text     "body"
     t.string   "author"
